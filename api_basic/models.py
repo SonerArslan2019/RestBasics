9158076b9f89890f44ccc04e0a2cf914f1f8fc4c
cprm <soner@arslanyapi.com.tr>
2021-04-24 16:18:25 +0300

    ikinci commit

--- api_basic/models.py
+++ api_basic/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    authot = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+
+    # class Meta:
+    #     db_table = 'article'
+    #     verbose_name = 'Article'
+    #     verbose_name_plural = 'Article'
+
+    def __str__(self):
+        return self.title
